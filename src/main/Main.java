85b19b50b7e6f6d770fc73b366ec87238d563020
Surade <shivam.surade@wescodist.com>
2024-10-21 18:28:53 +0530

    added to test2

---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+class Manin{
+    public static void main(String[] args) {
+        System.out.println("something");
+    }
+
+    public static void secondFunction(){
+        System.out.println("second");
+    }
+}
